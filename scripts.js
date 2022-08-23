70107014e64f11b3c95c5d7f127cf20cc04fe600
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-08-23 14:39:49 +0100

    search field implemented

--- scripts.js
+++ scripts.js
@@ -14,10 +14,26 @@ const secondButtons = document.querySelectorAll(".btn-group > button:nth-child(2
 const CARDS = document.querySelectorAll(".card")
 for (let i = 0; i < secondButtons.length; i++) {
     secondButtons[i].innerHTML = "Hide"
-    secondButtons[i].addEventListener("active", () => {
+    secondButtons[i].addEventListener("click", () => {
         CARDS[i].classList.add("d-none");
     })
 }
+
+const setButtons = document.querySelectorAll(".input-group-append > btn");
+
+const setQuery = num => {
+    if (num == 1) {
+        queryPrimary = document.getElementById("queryInput").value;
+        document.getElementById("queryInput").value = "";
+        alert("Query1 set to '" + queryPrimary + "'");
+    } else {
+        querySecondary = document.getElementById("queryInput").value;
+        document.getElementById("queryInput").value = "";
+        alert("Query2 set to '" + querySecondary + "'");
+    }
+}
+
+const picID = document.querySelectorAll(".text-muted")
 
 const loadPictures = (query) => {
   fetch("https://api.pexels.com/v1/search?query=" + query, options)
@@ -33,6 +49,7 @@ const loadPictures = (query) => {
         for (let i = 1; i < CARD_IMGS.length; i++) {
           const NEW_IMAGE = document.createElement("img");
           NEW_IMAGE.src = pictures[i].src.large;
+          picID[i+1].innerHTML = pictures[i].id.toString();
           CARD_IMGS[i].parentNode.replaceChild(NEW_IMAGE, CARD_IMGS[i]);
         }
         
@@ -40,9 +57,11 @@ const loadPictures = (query) => {
         const IMAGES = document.getElementsByTagName("img");
         for (let v = 0; v < IMAGES.length; v++) {
           IMAGES[v].src = pictures[v].src.large;
+          picID[v+2].innerHTML = pictures[v].id.toString();
         }
       }
       notBeenClicked = false;
+
     })
     .catch((err) => {
       console.log("ERROR:", err.message);
